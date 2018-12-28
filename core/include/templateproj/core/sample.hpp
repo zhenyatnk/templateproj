f7af85d2cef4428150a0d9e925cb866825454c6b
Zhenya <tinkalyk@mail.ru>
2018-12-28 11:28:07 +0300

    Create sample.hpp

---
+++ core/include/templateproj/core/sample.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <templateproj/core/BaseExceptions.hpp>
+#include <templateproj/core/export.hpp>
+
+namespace templateproj {
+namespace core {
+  
+void TEMPLATEPROJ_CORE_EXPORT SampleFunction(int i);
+
+}
+}
